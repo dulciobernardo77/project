74b51ae56ad09abee13df99e87cc51f10ba84cb7
Dulciobernard <dulciobernardo80@gmail.com>
2025-01-02 11:02:15 +0100

    Adiciona a página inicial com estrutura básica

---
+++ curso_de_js/conversor/assets/script.js
@@ -0,0 +1,10 @@
+let inputusd = window.document.querySelector('input#usd');
+let inputbrl = window.document.querySelecto('input#brl');
+
+inputusd.addEventListener('keyup', () => {
+    console.log('apertou no campo USD');
+});
+
+inputbrl.addEventListener('keyup', () => {
+    console.log('apertou no campo BRL');
+});
