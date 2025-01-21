a5328071566d4ac4df3d936b6b2f9e55fe0d1f02
Dulciobernard <dulciobernardo80@gmail.com>
2025-01-21 17:00:44 +0100

    exercicio sobre operadores condicionais

--- ESTUDO-JS/JSPractice/exefundamentos/Laco_de_Repeticao/fatorial.js
+++ ESTUDO-JS/JSPractice/exefundamentos/Laco_de_Repeticao/fatorial.js
@@ -1,7 +1,6 @@
 let factorial = 1;
 let number = 5;
-for(let counter = 1; counter <= number ;counter--){
+for(let counter = 1; counter <= number ;counter++){
 	factorial*=counter;
-	console.log(counter);
+	console.log(factorial);
 }
-console.log(factorial);
