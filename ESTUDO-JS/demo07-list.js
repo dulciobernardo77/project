62f4805561b31a1b09d5dcbf248af3887292d66d
dulciobernardo77 <dulciobernardo80@gmail.com>
2025-01-30 00:25:17 +0100

    Estudar arrays e resolver exercícios práticos
    - Praticado métodos como map, filter e reduce.
    - Resolvido exercícios de manipulação de arrays.

---
+++ ESTUDO-JS/demo07-list.js
@@ -0,0 +1,16 @@
+//Crie um array chamado numeros que contenha os números de 1 a 5.
+
+let num =[1,2,3,4,5];
+
+console.log(num);
+
+/**Acessando Elementos:
+Dado o array const cores = ["vermelho", "azul", "verde"];, exiba o segundo elemento no console. */
+
+let color = [
+    'Red',
+    'Blue',
+    'green'
+]; 
+
+console.log(color[2]);
