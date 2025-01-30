82c10ac5a7a7b2cf67f922c2d63e492dd0040242
dulciobernardo77 <dulciobernardo80@gmail.com>
2025-01-30 22:08:54 +0100

    Estudando array

--- ESTUDO-JS/demo07-list.js
+++ ESTUDO-JS/demo07-list.js
@@ -1,3 +1,45 @@
+const listadetarefas =[
+        "Arrumar o cama",
+        "correr ou caminha",
+        "Eviar email",
+        "Estudar"
+]
+
+//console.log(listadetarefas.length);
+//console.log(listadetarefas.push("Formatarcomputador"));
+//console.log(listadetarefas);
+
+//const ultimo = listadetarefas.pop();
+//console.log(ultimo,listadetarefas);
+
+const primeiro = listadetarefas.shift();
+console.log(primeiro,listadetarefas);
+
+console.log(listadetarefas);
+listadetarefas.splice(1,1);
+console.log(listadetarefas);
+
+
+
+const itens =[1,"computador",0.89];
+
+/*console.log(
+    typeof(itens)
+);*/
+
+//console.log(
+//    Array.isArray(itens)
+//);
+
+const ordenar = ["c","a","d","b","e"];
+//console.log(ordenar);
+//console.log(ordenar.sort());
+//console.log(ordenar.concat(['f','g','h','i']));
+//console.log(ordenar.join(','));
+
+
+
+/*
 //Crie um array chamado numeros que contenha os números de 1 a 5.
 
 let num =[1,2,3,4,5];
@@ -5,7 +47,7 @@ let num =[1,2,3,4,5];
 console.log(num);
 
 /**Acessando Elementos:
-Dado o array const cores = ["vermelho", "azul", "verde"];, exiba o segundo elemento no console. */
+Dado o array const cores = ["vermelho", "azul", "verde"];, exiba o segundo elemento no console. 
 
 let color = [
     'Red',
@@ -14,3 +56,17 @@ let color = [
 ]; 
 
 console.log(color[2]);
+
+*/
+/**
+ * Modificando Elementos:
+ * Dado let frutas = ["maçã", "banana", "uva"];, 
+ * substitua "banana" por "laranja".
+ */
+/*
+let fruit = [
+        "apple",
+        "banana",
+        "oragen"
+];
+*/
