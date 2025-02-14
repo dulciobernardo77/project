52f1f4cc646e8011414cd29322fa3fc5720fcecb
dulciobernardo77 <dulciobernardo80@gmail.com>
2025-02-14 23:33:55 +0100

    Verifica se uma string é um palíndromo.

---
+++ ESTUDO-JS/JSPractice/execboolean/palindromo.js
@@ -0,0 +1,7 @@
+// Verifica se uma string é um palíndromo.
+// Um palíndromo é uma palavra ou frase que, 
+// quando lida de trás para frente, permanece igual.
+// Exemplo: "radar", "ovo" e "arara" são palíndromos.
+
+
+//se str[loop]
